e226c298cdc65c0f1b10579bde05dc930cc3b4e7
yangyufei <yangyufei@zhihu.com>
2018-12-18 14:45:38 +0800

    remove info

--- app/src/androidTest/java/jarvis/com/nestedtouchscrollinglayout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jarvis/com/nestedtouchscrollinglayout/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("zhihu.com.nestedtouchscrollinglayout", appContext.getPackageName());
+        assertEquals("jarvis.com.nestedtouchscrollinglayout", appContext.getPackageName());
     }
 }
--- library/src/androidTest/java/jarvis/com/library/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/jarvis/com/library/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("zhihu.com.library.test", appContext.getPackageName());
+        assertEquals("jarvis.com.library.test", appContext.getPackageName());
     }
 }
